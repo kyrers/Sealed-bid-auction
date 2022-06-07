7dfc8b6eacc9074e65bf8dd613c8365d05dee2c1
kyrers <kyrers.eth@gmail.com>
2022-06-07 17:02:47 +0100

    Contract updates

--- packages/hardhat/contracts/AuctionHouse.sol
+++ packages/hardhat/contracts/AuctionHouse.sol
@@ -18,21 +18,36 @@ contract AuctionHouse is Ownable {
     //Mapping of users and their bid status
     mapping(address => bool) public isSealed;
 
-    //Highest bid
-    uint256 private highestBid;
-
-    //Limit for bids
-    uint256 public auctionDeadline;
-
-    //End of auction
-    uint256 public auctionEnd;
+    //Array of bidders
+    address[] bidders;
 
     //Highest bidder
     address private highestBidder;
 
+    //Highest bid
+    uint256 private highestBid;
+
+    //End of auction
+    uint256 public auctionEnd;
+
+    //Limit for openning bids
+    uint256 public openBidDeadline;
+
     //Auction status
     bool public isAuctionLive;
     
+    /*------------------------------------------------------------
+                                 MODIFIERS
+    --------------------------------------------------------------*/
+    modifier noLiveAuction() {
+        if (isAuctionLive) revert AuctionAlreadyLive();
+        _;
+    }
+
+    modifier liveAuction() {
+        if (!isAuctionLive) revert NoAuctionLive();
+        _;
+    }
     
     /*------------------------------------------------------------
                                  EVENTS
@@ -43,6 +58,8 @@ contract AuctionHouse is Ownable {
     /*------------------------------------------------------------
                                  ERRORS
     --------------------------------------------------------------*/
+    error AuctionAlreadyLive();
+    error NoAuctionLive();
     error NoFundsSent();
     error NotEnoughBalance();
     error FailedToSendFunds();
@@ -57,34 +74,47 @@ contract AuctionHouse is Ownable {
 
     /**
     * @notice Allows owner to start an auction
+    * @param _duration Duration of the auction in minutes
     */
-    function startAuction(uint256 _end) external onlyOwner {
-        
+    function startAuction(uint256 _duration) external onlyOwner noLiveAuction {
+        //Delete mappings
+        resetMappings();
+
+        //Update timestamps
+        auctionEnd = block.timestamp + _duration * 1 minutes;
+        openBidDeadline = auctionEnd + 5 minutes;
+
+        //Update auction info
+        isAuctionLive = true;
+        highestBidder = address(0);
+        highestBid = type(uint256).max;
     }
 
     /**
     * @notice Allow user to make a bid
     * @dev If the highest bid is equal to msg.value, the older bid will stay as the highest
     */
-    function placeBid() external payable {
+    /*function placeBid() external payable liveAuction {
         //Check no value sent
         if (msg.value <= 0) revert NoFundsSent();
 
+        //Update user balance
         balances[msg.sender] = msg.value;
         
+        //Check if highest bid
         if(msg.value > highestBid) {
             highestBid = msg.value;
             highestBidder = msg.sender;
         }
 
         emit BidPlaced(msg.sender, msg.value);
-    }
+    }*/
 
     /**
     * @notice Allow user to withdraw
     * @param _amount Amount to withdraw
     */
-    function openBid(uint256 _amount) external payable {
+    /*function openBid(uint256 _amount) external payable {
         //Check no value sent
         if (balances[msg.sender] < _amount) revert NotEnoughBalance();
 
@@ -96,5 +126,17 @@ contract AuctionHouse is Ownable {
         if(!success) revert FailedToSendFunds();
  
         emit Withdrawal(msg.sender, msg.value);
+    }*/
+
+    /**
+    * @notice Reset mappings and bidders array upon new auction start
+    */
+    function resetMappings() internal {
+        for (uint i=0; i< bidders.length ; i++){
+            delete balances[bidders[i]];
+            delete isSealed[bidders[i]];
+        }
+
+        delete bidders;
     }
 }
