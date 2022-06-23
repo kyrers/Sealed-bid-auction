f2abfd795f4f2e8dcc2c61cdaa8c40e201e576db
kyrers <kyrers.eth@gmail.com>
2022-06-23 01:31:33 +0100

    Connect to wallet and contract

--- packages/frontend/auction_house/src/App.tsx
+++ packages/frontend/auction_house/src/App.tsx
@@ -1,10 +1,12 @@
 import { JsonRpcSigner } from '@ethersproject/providers';
 import { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
 import { connect } from './hooks/connect';
 import { loadContract } from './hooks/loadContract';
+import { targetNetwork } from './config/config';
 import './App.css';
 
-function App() {  
+function App() {
   const [userSigner, setUserSigner] = useState<JsonRpcSigner | null>();
   const [connectedWallet, setConnectedWallet] = useState("");
 
@@ -28,17 +30,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Auction House</h1>
+        <h5 className="header-target-network">{targetNetwork.name}</h5>
+        <Button onClick={connect}>
+          {
+            connectedWallet !== "" ?
+              <span>{connectedWallet}</span>
+              :
+              <span>Connect</span>
+          }
+        </Button>
       </header>
     </div>
   );
