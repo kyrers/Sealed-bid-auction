472794ad162c5041db5f29cfca3519b8c8ad208e
kyrers <kyrers.eth@gmail.com>
2022-07-02 00:27:41 +0100

    Refactored Main Panel UI before it got  complex. It was spaghetti

--- packages/frontend/auction_house/src/components/MainPanel.tsx
+++ packages/frontend/auction_house/src/components/MainPanel.tsx
@@ -6,52 +6,82 @@ type FunctionProps = {
     openBidDeadline: number;
 };
 
-function MainPanel({ auctionEnd, openBidDeadline }: FunctionProps) {
+type RenderCardFunctionProps = {
+    placeBidDisabled: boolean;
+    liveAuction: boolean;
+    openBidDisabled: boolean;
+};
 
-    if (auctionEnd === 0 && openBidDeadline === 0) {
-        return (
-            <Row className="main-panel">
-                <Col sm={4}>
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Place bid</h5>
-                            <Button active={false} className="custom-button">
+function MainPanel({ auctionEnd, openBidDeadline }: FunctionProps) {
+    var currentDate = new Date().getTime();
+
+    //if (auctionEnd < currentDate && openBidDeadline < currentDate) { // No auction live
+    if (auctionEnd >= currentDate) { // Within bidding period
+        return <RenderCards placeBidDisabled={true} liveAuction={true} openBidDisabled={true} />
+    }
+
+    if (auctionEnd < currentDate && openBidDeadline >= currentDate) { // Within open bid deadline
+        return <RenderCards placeBidDisabled={false} liveAuction={true} openBidDisabled={true} />
+    }
+
+    return <RenderCards placeBidDisabled={true} liveAuction={false} openBidDisabled={true} />
+}
+
+function RenderCards({ placeBidDisabled, liveAuction, openBidDisabled }: RenderCardFunctionProps) {
+    return (
+        <Row className="main-panel">
+            <Col sm={4}>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Place bid</h5>
+                        <div className="card-info">
+                            <Button disabled={placeBidDisabled} className="custom-button">
                                 <span>Place Bid</span>
                             </Button>
                         </div>
                     </div>
-                </Col>
-                <Col sm={4}>
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Current Auction</h5>
-                            <span>No Auction Live</span>
-                            <Button active={false} className="custom-button">
+                </div>
+            </Col>
+            <Col sm={4}>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Current Auction</h5>
+                        <div className="card-info">
+                            {
+                                liveAuction ?
+
+                                    <span> Auction Live</span>
+                                    :
+                                    <span>No Auction Live</span>
+                            }
+                            <Button disabled={liveAuction} className="custom-button">
                                 <span>Start auction</span>
                             </Button>
                         </div>
                     </div>
-                </Col>
-                <Col sm={4}>
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Open Bid</h5>
-                            <Button active={false} className="custom-button">
-                                <span>Place Bid</span>
+                </div>
+            </Col>
+            <Col sm={4}>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Open Bid</h5>
+                        <div className="card-info">
+                            <Button disabled={openBidDisabled} className="custom-button">
+                                <span>Open Bid</span>
                             </Button>
                         </div>
                     </div>
-                </Col>
-            </Row >
-        );
-    }
-    return (
-        <div className="main-panel">
-            <span>Auction Live</span>
-            <span>Auction End: {new Date(auctionEnd).toLocaleString()}</span>
-            <span>Open Bid Deadline {new Date(openBidDeadline).toLocaleString()}</span>
-        </div>
+                </div>
+            </Col>
+        </Row >
     );
 }
+/*return (
+    <div className="main-panel">
+        <span>Auction Live</span>
+        <span>Auction End: {new Date(auctionEnd).toLocaleString()}</span>
+        <span>Open Bid Deadline {new Date(openBidDeadline).toLocaleString()}</span>
+    </div>
+);*/
 
 export default MainPanel;
